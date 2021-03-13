763f1997c60faa2de6d1e881e3a4d3f0673baab9
ninjapretzel <ninjapretzel@gmail.com>
2021-03-13 18:03:01 -0500

    Starts adding parser

---
+++ Lib/Helpers.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ni_compiler {
+
+	public static class Helpers {
+		/// <summary> Checks the string starting at position = startIndex to see if check directly matches the string from that position. </summary>
+		/// <param name="str"> String to check </param>
+		/// <param name="startIndex"> first index in str to match against first char in check </param>
+		/// <param name="check"> string to check against str </param>
+		/// <returns> true if, from startIndex for check.Length characters, str contains the same characters as check does. 
+		/// False if str runs out of characters, or has any characters that don't match the parallel ones in check.</returns>
+		internal static bool MatchAt(this string str, int startIndex, string check) {
+			for (int i = 0; i < check.Length; i++) {
+				int idx = i + startIndex;
+				if (idx > str.Length) { return false; }
+				if (str[idx] != check[i]) { return false; }
+			}
+			return true;
+		}
+
+		/// <summary> Checks the string for whitespace at a given index. Outside of the string is considered whitespace. </summary>
+		/// <param name="str"> String to check </param>
+		/// <param name="i"> index to check at </param>
+		/// <returns> True if whitespace is at the index or index is invalid, false otherwise. </returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static bool WhitespaceAt(this string str, int i) {
+			if (i < 0 || i >= str.Length) { return true; }
+			char c = str[i];
+			return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+		}
+
+		/// <summary> Checks the string for an alphanumeric character at a given index. </summary>
+		/// <param name="str"> String to check </param>
+		/// <param name="i"> index to check at </param>
+		/// <returns> True if there is an alphanumeric characater at the index, false otherwise. </returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static bool AlphaNumAt(this string str, int i) {
+			if (i < 0 || i >= str.Length) { return false; }
+			char c = str[i];
+			return ((c >= 'a' && c <= 'z')
+				|| (c >= 'A' && c <= 'Z')
+				|| (c >= '0' && c <= '9'));
+		}
+
+		/// <summary> Strips standard C-Style comments from text </summary>
+		/// <param name="input"> Input text file to strip comments from </param>
+		/// <returns> Modified string without comment blocks </returns>
+		public static string StripCStyleComments(string input) {
+			StringBuilder output = new StringBuilder();
+
+			const int NORMAL = 0;
+			const int LINE_COMMENT = 1;
+			const int BLOCK_COMMENT = 2;
+			const int STRING = 3;
+
+			int mode = NORMAL;
+			for (int i = 0; i < input.Length; i++) {
+				char c = input[i];
+				if (mode == NORMAL) {
+					if (c == '/') {
+						char c2 = input[i + 1];
+						if (c2 == '/' || c2 == '*') {
+							mode = c2 == '/' ? LINE_COMMENT : BLOCK_COMMENT;
+							i++;
+							continue;
+						}
+					} else if (c == '"') {
+						mode = STRING;
+					}
+					output.Append(c);
+
+				} else if (mode == LINE_COMMENT) {
+
+					if (c == '\n') {
+						mode = NORMAL;
+						output.Append('\n');
+					}
+
+				} else if (mode == BLOCK_COMMENT) {
+
+					if (c == '*') {
+						char c2 = input[i + 1];
+						if (c2 == '/') {
+							i++;
+							mode = NORMAL;
+						}
+					} else if (c == '\n') { output.Append('\n'); }
+					// Preserve line numbers
+
+				} else if (mode == STRING) {
+
+					if (c == '"') {
+						char cp = input[i - 1];
+						if (cp != '\\') {
+							mode = NORMAL;
+						}
+					}
+
+					output.Append(c);
+				}
+
+
+			}
+
+
+			return output.ToString();
+		}
+	}
+}
--- N1.cs
+++ N1.cs
@@ -4,11 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ni_compiler {
 
-	public class N1Lang {
+	public static class N1Lang {
 
 		public enum N1 : int {
 			Int, Read, Negate, Add,
@@ -205,6 +206,48 @@ namespace ni_compiler {
 		}
 
 		public class _Tests {
+			static string prog = @"
+let ni 
+	y 
+is 
+	let ni 
+		x1 
+	is 
+		20
+	in
+		x1 + (let ni x2 is 22)
+	end
+in 
+	y
+end";
+			public static void TestTokenize() {
+				Tokenizer tok = new Tokenizer(prog);
+				Token t;
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("let");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("ni");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NAME"); t.content.ShouldBe("y");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("is");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("let");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("ni");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NAME"); t.content.ShouldBe("x1");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("is");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NUM"); t.content.ShouldBe("20");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("in");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NAME"); t.content.ShouldBe("x1");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("+");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("(");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("let");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("ni");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NAME"); t.content.ShouldBe("x2");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("is");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NUM"); t.content.ShouldBe("22");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe(")");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("end");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("in");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("!NAME"); t.content.ShouldBe("y");
+				t = tok.peekToken; tok.Next();t.type.ShouldBe("end");
+				
+			}
 			/// <summary> Test cases from professor </summary>
 			public static void TestUniquify() {
 				void Verify(string note, Node a, Node b) {
@@ -418,6 +461,285 @@ namespace ni_compiler {
 
 
 		}
+
+		/// <summary> Impossible token representing type for names </summary>
+		public const string NAME = "!NAME";
+		/// <summary> Impossible token representing type for numbers </summary>
+		public const string NUMBER = "!NUM";
+		/// <summary> Impossible token representing type for strings</summary>
+		public const string STRING = "!STR";
+		/// <summary> type for names </summary>
+		public const string SPACE = " ";
+		/// <summary> type for names </summary>
+		public const string NEWLINE = "\n";
+		/// <summary> type for names </summary>
+		public const string TAB = "\t";
+		/// <summary> Keywords of the language </summary>
+		public static readonly string[] keywords = {
+			"let", "ni", "is", "in", "end", "read",
+		};
+
+		/// <summary> Punctuation of the language, with larger constructs checked first. </summary>
+		public static readonly string[] punct = {
+			// Size 3
+			
+			// Size 2
+
+			// Size 1
+			"+", "-", "(", ")"
+		};
+		/// <summary> Regex pattern for matching names </summary>
+		public const string nameRegex = @"[a-zA-Z_\$][a-zA-Z0-9_\$]*";
+		/// <summary> Regex checker for names </summary>
+		public static readonly Regex name = new Regex(nameRegex);
+
+		/// <summary> Regex pattern for matching numbers </summary>
+		public const string numRegex = @"(0x[0-9A-Fa-f]+[lL]?)|(\d+\.\d*[fF]?)|(\d*\.\d+[fF]?)(0x[0-9A-Fa-f]+[lL]?)|(\d+[lL]?)|(\d+\.\d*[fF]?)|(\d*\.\d+[fF]?)";
+		/// <summary> Regex checker for numbers </summary>
+		public static readonly Regex num = new Regex(numRegex);
+
+
+		public static Node ParseProgram(this Tokenizer tok) {
+			return tok.ParseExpression();
+		}
+
+		public static string[] LEAFS = { "!NAME", "!NUM", "read" };
+
+		public static Node ParseExpression(this Tokenizer tok) {
+			if (tok.At("let")) { return tok.ParseLet(); }
+			if (tok.At("(")) {
+				tok.Next();
+				Node inner = tok.ParseExpression();
+				tok.RequireNext(")");
+				return inner;
+			}
+			if (tok.At("-")) {
+				tok.Next();
+				Node negate = new Node(N1.Negate.Ord());
+				Node expr = tok.ParseExpression();
+				negate.List(expr);
+				return negate;
+
+			}
+			if (tok.At(LEAFS)) {
+				Node leaf = tok.ParseLeaf();
+				if (tok.At("+")) {
+					
+				}
+			}
+			tok.Error($"Unknown start of expression {tok.peekToken}");
+			return null;
+		}
+		public static Node ParseLeaf(this Tokenizer tok) {
+			if (tok.At("!NAME")) {
+				Node var = new Node(N1.Var.Ord());
+				var.List(tok.Next());
+				return var;
+			}
+			if (tok.At("!NUM")) {
+				Node num = new Node(N1.Int.Ord());
+				num.List(tok.Next());
+				return num;
+			}
+			if (tok.At("read")) {
+				return new Node(N1.Read.Ord());
+			}
+			tok.Error("Unknown leaf value token");
+			return null;
+		}
+		public static Node ParseLet(this Tokenizer tok) {
+			tok.RequireNext("let");
+			tok.RequireNext("ni");
+			tok.Require("!NAME");
+			
+			Token name = tok.Next();
+			tok.RequireNext("is");
+
+			Node value = tok.ParseExpression();
+			Node expr;
+			if (tok.At("in")) {
+				tok.RequireNext("in");
+				expr = tok.ParseExpression();
+				tok.RequireNext("end");
+			} else {
+				expr = new Node(N1.Var.Ord());
+				expr.List(name);
+			}
+
+			Node let = new Node(N1.Let.Ord());
+			let.List(name);
+			let.List(value);
+			let.List(expr);
+
+			return let;
+		}
+
+		/// <summary> Represents a stream of tokens coming from a loaded script </summary>
+		public class Tokenizer {
+
+			/// <summary> Original source string </summary>
+			public string src { get; private set; }
+
+			/// <summary> Current column </summary>
+			public int col { get; private set; }
+			/// <summary> Current line </summary>
+			public int line { get; private set; }
+			/// <summary> Current raw char position </summary>
+			public int i { get; private set; }
+
+
+			/// <summary> Token ahead of cursor, which hasn't been consumed. </summary>
+			public Token peekToken { get; private set; }
+			/// <summary> Last consumed token </summary>
+			public Token lastToken { get; private set; }
+			/// <summary> Last consumed non-whitespace token </summary>
+			public Token lastRealToken { get; private set; }
+
+			/// <summary> Gets the content from the current peek token </summary>
+			public string content { get { return peekToken.content; } }
+
+			/// <summary> Gets the type from the current peek token </summary>
+			public string atType { get { return peekToken.type; } }
+
+
+			/// <summary> Basic Constructor.</summary>
+			/// <param name="source"> Source file to read. </param>
+			public Tokenizer(string source) {
+				src = source.Replace("\r\n", "\n").Replace("\r", "\n");
+				Reset();
+			}
+
+			/// <summary> Throw an exception with a given message </summary>
+			/// <param name="message"></param>
+			public void Error(string message) { throw new Exception(message + "\n" + this); }
+
+			/// <summary> Throws an exception if the peekToken is NOT the given type. </summary>
+			public void Require(string type) { if (!peekToken.Is(type)) { Error("Expected: " + type); } }
+
+			/// <summary> Throws an exception if the peekToken is NOT one of the given types. </summary>
+			public void Require(string[] types) {
+				if (!peekToken.Is(types)) {
+					string str = "";
+					foreach (string s in types) { str += s + ", "; }
+					Error("Expected " + str);
+				}
+			}
+
+			/// <summary> Throws an exception if the peekToken is NOT the given type, 
+			/// but if it is, consumes it. </summary>
+			public void RequireNext(string type) { Require(type); Next(); }
+
+			/// <summary> Throws an exception if the peekToken is NOT one of the given types, 
+			/// but if it is, consumes it. </summary>
+			public void RequireNext(string[] types) { Require(types); Next(); }
+
+
+			/// <summary> Returns if the peekToken is a given type </summary>
+			public bool At(string type) { return peekToken.Is(type); }
+
+			/// <summary> Returns if the peekToken is one of a given set of types </summary>
+			public bool At(string[] types) { return peekToken.Is(types); }
+
+			/// <summary> Returns true if the Tokenizer is out of tokens. </summary>
+			public bool Done { get { return !peekToken.IsValid; } }
+
+			/// <summary> Resets the tokenizer to its initial state </summary>
+			public void Reset() {
+				i = 0;
+				line = 1;
+				col = 0;
+				lastRealToken = lastToken = Token.INVALID_TOKEN;
+				peekToken = Peek();
+				while (peekToken.IsWhitespace) {
+					Move();
+				}
+			}
+
+
+			/// <summary> Get the next token </summary>
+			/// <returns> Token that has been removed from context, or the current token if invalid. </returns>
+			public Token Next() {
+				if (peekToken.IsValid) {
+					Token save = lastRealToken;
+
+					if (Move()) {
+						while (peekToken.IsValid && peekToken.IsWhitespace) { Move(); }
+					}
+
+					return save;
+				}
+
+				return peekToken;
+			}
+
+			/// <summary> Moves this tokenizer forward by one token. </summary>
+			/// <returns> True if moved at all, false if nothing happened. </returns>
+			public bool Move() {
+				if (peekToken.IsValid) {
+					if (peekToken.Is(NEWLINE)) { col = 0; line++; } else { col += peekToken.content.Length; }
+
+					i += peekToken.content.Length;
+					lastToken = peekToken;
+					if (!peekToken.IsWhitespace) { lastRealToken = peekToken; }
+					peekToken = Peek();
+				}
+				return peekToken.IsValid;
+			}
+
+			/// <summary> Peeks ahead at the next token that has yet to be consumed. </summary>
+			/// <returns> Next token sitting in front of the head, or an invalid token if out of characters or WTF. </returns>
+			public Token Peek() {
+				if (i >= src.Length) { return Token.Done(line, col); }
+				// Whitespace
+				char c = src[i];
+				if (c == ' ') { return new Token(SPACE, line, col); }
+				if (c == '\t') { return new Token(TAB, line, col); }
+				if (c == '\n') { return new Token(NEWLINE, line, col); }
+				if (c == '\"') { return ExtractString('\"'); }
+				if (c == '\'') { return ExtractString('\''); }
+				foreach (string p in punct) { if (src.MatchAt(i, p)) { return new Token(p, line, col); } }
+				foreach (string k in keywords) { if (src.MatchAt(i, k) && !src.AlphaNumAt(i + k.Length)) { return new Token(k, line, col); } }
+
+				Match nameCheck = name.Match(src, i);
+				if (nameCheck.Success && nameCheck.Index == i) { return new Token(nameCheck.Value, NAME, line, col); }
+
+				Match numCheck = num.Match(src, i);
+				if (numCheck.Success && numCheck.Index == i) { return new Token(numCheck.Value, NUMBER, line, col); }
+
+				return Token.Invalid(line, col);
+			}
+
+			/// <summary> Error token message if a newline is sitting inside of a string literal.</summary>
+			public static readonly string BAD_STRING_NEWLINE_INSIDE = "Newline in string literal";
+			/// <summary> Error token message if no matching character for a string delimeter. </summary>
+			public static readonly string BAD_STRING_NO_MATCHING_QUOTE = "No matching quote for string literal";
+
+			/// <summary> Extracts a string from the current position in the source </summary>
+			/// <param name="match"> Character to match on the other end of the string region </param>
+			/// <returns> Token created from matched string, or an error token if it FAILED. </returns>
+			private Token ExtractString(char match) {
+				// Find next actual newline
+				int nextNL = src.IndexOf('\n', i + 1);
+				// Make index always greater than any characters in the string.
+				if (nextNL == -1) { nextNL = src.Length + 1; }
+
+				int nextMatch = src.IndexOf(match, i + 1);
+				while (true) {
+					if (nextMatch == -1) { return new Token(BAD_STRING_NO_MATCHING_QUOTE, Token.INVALID, line, col); }
+					if (nextMatch > nextNL) { return new Token(BAD_STRING_NEWLINE_INSIDE, Token.INVALID, line, col); }
+					if (src[nextMatch - 1] != '\\') { break; }
+					nextMatch = src.IndexOf(match, nextMatch + 1);
+				}
+
+				int len = nextMatch - i + 1;
+				return new Token(src.Substring(i, len), STRING, line, col);
+			}
+
+			public override string ToString() {
+				return "Token: " + peekToken + " on Line: " + line + " Col " + col;
+			}
+		}
 	}
+	
 
 }
